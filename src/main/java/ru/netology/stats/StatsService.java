7ccc9831acc49732ae2308d1aa7f9fe1a6003ba3
OlgaDanilovaV <danilovaolga2235@gmail.com>
2020-05-25 17:42:22 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,69 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public int calculateSum(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+    public int findAverage(int[] sales){
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        int currentAverage = sum / sales.length;
+        return currentAverage;
+    }
+    public int findMonthMax(int[] sales){
+        int currentMax = sales[0];
+        int month = 0;
+        int monthMax = 1;
+        for (int sale : sales) {
+            month++;
+            if (currentMax <= sale) {
+                currentMax = sale;
+                monthMax = month;
+            }
+        }
+        return monthMax;
+    }
+    public int findMonthMin(int[] sales) {
+        int currentMin = sales[0];
+        int month = 0;
+        int monthMin = 0;
+        for (int sale : sales) {
+            month++;
+            if (currentMin > sale) {
+                currentMin = sale;
+                monthMin = month;
+            }
+        }
+        return monthMin;
+    }
+    public int findCountOverAverage(int[] sales) {
+        int currentAverage = findAverage(sales);
+        int count = 0;
+        int countOverAverage = 0;
+        for (int sale : sales) {
+            if (currentAverage < sale) {
+                count++;
+                countOverAverage = count;
+            }
+        }
+        return countOverAverage;
+    }
+    public int findCountUnderAverage(int[] sales) {
+        int currentAverage = findAverage(sales);
+        int count = 0;
+        int countUnderAverage = 0;
+        for (int sale : sales) {
+            if (currentAverage > sale) {
+                count++;
+                countUnderAverage = count;
+            }
+        }
+        return countUnderAverage;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,74 @@
+package ru.netology.stats;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateSum() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.calculateSum(sales);
+
+        assertEquals(expected, actual);
+
+    }
+    @org.junit.jupiter.api.Test
+    void findAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.findAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void findMonthMax() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = service.findMonthMax(sales);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void findMonthMin() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.findMonthMin(sales);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void findCountOverAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.findCountOverAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void findCountUnderAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.findCountUnderAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+}
